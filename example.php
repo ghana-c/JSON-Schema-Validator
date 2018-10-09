b41db9a4e0b2f2067dc963401b43544d87505e79
Ghanashyam Chaudhari <33869773+ghana-c@users.noreply.github.com>
2018-10-09 15:00:48 +0530

    Example of JSON schema validator

--- example.php
+++ example.php
@@ -1,76 +1,76 @@
 <?php
 
-	/* Include JSON Formatter class file */
-	include("JsonFormatter.php");
+	/* Include JSON Schema validator class file */
+	include("JsonSchemaValidator.php");
 	
 	/* Products data (dummy data) */
 	$products_data = '[{
-											"id": "CT9871",
-											"products": [
-												{
-													"product_id": "47586"
-												},
-												{
-													"product_id": null
-												},
-												{
-													"product_id": 47587
-												}
-											],
-											"links": {
-												"sandbox": {
-												  "method": "GET",
-												  "url": "http://localhost/"
-												}
-											},
-											"categories": [
-												{
-													"id": 1,
-													"sub_category_ids": [896,897,898]
-												}
-											]
-										},
-										{
-											"id": "CT9872",
-											"products": [
-												{
-													"product_id": 47596
-												},
-												{
-													"product_id": 47597
-												},
-												{
-													"product_id": 47598
-												}
-											],
-											"codes": [589,590,591]
-										},
-										{
-											"id": "CT9873",
-											"products": [
-												{
-													"product_id": "47606"
-												}
-											],
-											"codes": [258,259,260]
-										}
-									]';
+				"id": "CT9871",
+				"products": [
+					{
+						"product_id": "47586"
+					},
+					{
+						"product_id": null
+					},
+					{
+						"product_id": 47587
+					}
+				],
+				"links": {
+					"sandbox": {
+					  "method": "GET",
+					  "url": "http://localhost/"
+					}
+				},
+				"categories": [
+					{
+						"id": 1,
+						"sub_category_ids": [896,897,898]
+					}
+				]
+			},
+			{
+				"id": "CT9872",
+				"products": [
+					{
+						"product_id": 47596
+					},
+					{
+						"product_id": 47597
+					},
+					{
+						"product_id": 47598
+					}
+				],
+				"codes": [589,590,591]
+			},
+			{
+				"id": "CT9873",
+				"products": [
+					{
+						"product_id": "47606"
+					}
+				],
+				"codes": [258,259,260]
+			}
+		]';
 
 	try {
 
-		$json_formatter = new JsonFormatter(true);
+		$json_sv = new JsonSchemaValidator(true);
 		
 		/* category_id_format: Any string starting with alphabets CT followed by numeric digits */
 		$extra_formats = [
 			'category_id_format' => '^CT\d+$'
 		];
-		/* Set extra formats in JSON formatter's class variable using method "setExtraFormats" */
-		$json_formatter->setExtraFormats($extra_formats);
+		/* Set extra formats in class variable of JSON schema validator using method "setExtraFormats" */
+		$json_sv->setExtraFormats($extra_formats);
 
 		$preformatted_json = file_get_contents('./json_preformats/products.json');
 
 		$error = '';
-		$result = $json_formatter->compile($products_data, $preformatted_json, $error, 'string');
+		$result = $json_sv->compile($products_data, $preformatted_json, $error, 'string');
 		
 		if(!empty($result)) {
 			echo "Response: <br/>";
